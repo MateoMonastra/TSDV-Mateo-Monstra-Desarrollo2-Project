c881f9dd2658c44a91e851287b517c00da2f909b
MateoMonastra <mateomonastravm@gmail.com>
2024-05-22 21:27:55 -0300

    fixed PlayerRotation with Joystick

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -14,6 +14,8 @@ namespace Player
         private float _yRotation;
         private float _xRotation;
 
+        private Vector2 _moveDirection;
+
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -22,30 +24,23 @@ namespace Player
 
         private void Update()
         {
+            _xRotation += _moveDirection.x;
+
+            _yRotation -= _moveDirection.y;
+            _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
+
             transform.rotation = Quaternion.Euler(_yRotation, _xRotation, 0);
             orientation.rotation = Quaternion.Euler(0, _xRotation, 0);
         }
 
         public void UpdateMouseCamera(Vector2 angle)
         {
-            float mouseX = angle.x * sensX;
-            float mouseY = angle.y * sensY;
-
-            _xRotation += mouseX;
-
-            _yRotation -= mouseY;
-            _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
+            _moveDirection = new Vector2(angle.x * sensX, angle.y * sensY);
         }
-        
+
         public void UpdateJoystickCamera(Vector2 axis)
         {
-            float joystickX = axis.x * sensX * Time.deltaTime;
-            float joystickY = axis.y * sensY * Time.deltaTime;
-
-            _xRotation += joystickX;
-
-            _yRotation -= joystickY;
-            _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
+            _moveDirection = new Vector2(axis.x * sensX * Time.deltaTime, axis.y * sensY * Time.deltaTime);
         }
     }
 }
