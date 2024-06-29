47993dbf8c6f34a813461cde3fac071fd1e0cf3e
MateoMonastra <mateomonastravm@gmail.com>
2024-06-29 20:20:14 -0300

    added addedTime Alert

--- Assets/Scripts/Gameplay/Timer/Timer.cs
+++ Assets/Scripts/Gameplay/Timer/Timer.cs
@@ -1,25 +1,37 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace LevelManager
 {
     public class Timer : MonoBehaviour
     {
-        private float _timer;
+        [Header("Alpha fade out effect")] 
+        [SerializeField] private Color newColor;
+        [SerializeField] private Color transparentColor;
+
+        [SerializeField] private float animFadeDuration;
+        [SerializeField] private float animOutDuration;
 
         public TextMeshProUGUI textTimer;
+        public TextMeshProUGUI addTimeText;
 
-        public float TotalTime => _timer;
+        private bool fading;
         
+        private float _timer;
+        public float TotalTime => _timer;
+
         private void Start()
         {
             _timer = 0;
         }
+
         private void Update()
         {
             _timer += Time.deltaTime;
-            
+
             int minutes = (int)_timer / 60;
             int seconds = (int)_timer % 60;
 
@@ -33,9 +45,39 @@ namespace LevelManager
             }
         }
 
+        private IEnumerator FadeInOut()
+        {
+            float startTime = Time.time;
+            float timer = 0;
+            
+            while (timer < animOutDuration && !fading)
+            {
+                timer = Time.time - startTime;
+                
+                addTimeText.color = Color.Lerp(addTimeText.color, newColor, timer/animOutDuration);
+                
+                yield return null;
+            }
+            
+            startTime = Time.time;
+            timer = 0;
+            fading = true;
+            
+            while (timer < animFadeDuration)
+            {
+                timer = Time.time - startTime;
+                addTimeText.color = Color.Lerp(addTimeText.color, transparentColor, timer/animOutDuration);
+                yield return null;
+            }
+
+        }
+        
+
         public void AddTime(float timeToAdd)
         {
             _timer += timeToAdd;
+            fading = false;
+            StartCoroutine(FadeInOut());
         }
     }
 }
